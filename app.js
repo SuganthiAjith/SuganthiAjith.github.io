56ed11bdb71ce1796e0123d599225a895b616b52
SuganthiAjith <s.suganthinathan20@gmail.com>
2020-10-21 06:52:01 -0700

    reverse of an array

---
+++ app.js
@@ -0,0 +1,20 @@
+//reverse of a array
+var a=[1,5,6,7,9];
+var i;
+i=a.reverse();
+console.log(i);
+//tree height program
+/*var i=0,s=1,x=15;
+while(i!==x)
+{
+    s=s*2;
+    s=s+1;
+    i++;
+}
+console.log(s);*/
+
+
+
+
+
+
