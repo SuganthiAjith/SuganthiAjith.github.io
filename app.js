9308baced30bd8ca9bcbc20f2d05e481c058f4ef
SuganthiAjith <s.suganthinathan20@gmail.com>
2020-10-21 06:59:36 -0700

    print an array

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //reverse of a array
 var a=[1,5,6,7,9];
 var i;
-i=a.reverse();
-console.log(i);
+//i=a.reverse();
+//console.log(i);
 //tree height program
 /*var i=0,s=1,x=15;
 while(i!==x)
@@ -12,7 +12,11 @@ while(i!==x)
     i++;
 }
 console.log(s);*/
-
+//print an array vertically
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}
 
 
 
