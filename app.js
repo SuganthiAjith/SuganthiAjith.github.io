515fc167c5409fa100944b4eb7e57ac75246b393
SuganthiAjith <s.suganthinathan20@gmail.com>
2020-10-21 20:51:56 -0700

    gang step1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //reverse of a array
-var a=[1,5,7,9];// b=[3,5,7,8];
+//var a=[1,5,7,9];// b=[3,5,7,8];
 //i=a.reverse();
 //console.log(i);
 //tree height program
@@ -15,11 +15,9 @@ console.log(s);*/
 //a.pop();
 //a.push(5);
 //a.unshift(10);
-a.shift();
+//a.shift();
 //for(i=0;i<a.length;i++)
-{
-    console.log(a);
-}
+   // console.log(a);
 //i=a.join("*");
 /*i=a.concat(b);
 console.log(i);
@@ -30,11 +28,15 @@ i=a.splice(2,0,8,10);*/
 //i=a.sort();
 //i=a.lastIndexOf(5);
 //i=a.includes(5,2);
-
 //console.log(i);
-
-
-
+var x1=2, x2=0, j1=1, j2=2;
+while(x1!=x2)
+{
+    console.log("not yet");
+    x1=x1+j1;
+    x2=x2+j2;
+}
+console.log("meet");
 
 
 
